8f88dee6d66a992d15e0108c86cfb1933d11b233
Lekuro <lekurot@gmail.com>
2021-10-09 00:06:38 +0300

    add user to chat

--- chat_django/chat/urls.py
+++ chat_django/chat/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # path('login', views.login, name='login'),
     path('', views.index_view, name='index'),
+    path('<int:room_id>/users/', views.invite_user, name='invite_user_endpoint'),
     path('<int:room_id>/', views.room_view, name='room_endpoint'),
 ]
--- chat_django/chat/views.py
+++ chat_django/chat/views.py
@@ -18,10 +18,25 @@ def index_view(request):
 @login_required
 def room_view(request, room_id):
     room = ChatRoom.objects.get(pk=room_id)
+    # not_invited_users = User.objects.all()
+    not_invited_users = []
+    for user in User.objects.all():
+        if not user.chat_rooms.filter(id=room_id).exists():
+            not_invited_users.append(user)
     user_has_access = room.room_users.filter(pk=request.user.id).exists()
     if user_has_access:
         return render(request, 'chat/room.html', {
             'room': room,
+            'not_invited_users': not_invited_users
         })
     else:
         return redirect('/chat')
+
+
+def invite_user(request, room_id):
+    invited_user_id = request.POST['invited_user_id']
+    invited_user = User.objects.get(id=invited_user_id)
+    room = ChatRoom.objects.get(id=room_id)
+    if not room.room_users.filter(id=invited_user_id).exists():
+        room.room_users.add(invited_user)
+    return redirect('/chat')
